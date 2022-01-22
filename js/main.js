820a0b58463f791172e53ce174139449c43015c6
delllc <s.shahmatov2014@yandex.ru>
2022-01-22 23:53:45 +0500

    main

--- js/main.js
+++ js/main.js
@@ -5,4 +5,13 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 3000,
     });
+
+
+$('.works__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="../icons/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="../icons/arrow-right.svg" alt=""></button>',
+})
+
 });
